72f6e2dec0c5501040739dd9a51373afd2e02f63
AleenaMad <aleena.madni@gmail.com>
2017-12-26 17:44:08 -0600

    added invaders, maybe movement?

--- ship.js
+++ ship.js
@@ -3,12 +3,8 @@ function ship () {
   // put the ship in the middle.
   this.x = width/2;
 
-
-
-
   this.show = function () {
     rect(this.x, height-20, 20, 60);
-    fill(blue);
   }
 
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,80 @@
-// Need to create the main variables and a box to set up the Game
-var ship;
-var invaders; = [];
-function setup () {
-  createCanvas(600,400);
+////////////////////////////////////////////
+//////////Testing Area////////////////////
+
+//
+// var element2 = document.createElement('p')
+// element2.innerHTML = "hello world";
+// console.log(element2);
+// var element = document.getElementById('ship');
+// element.append(element2);
 
 
+////////////////////////////////////////////
+//////////Testing Area////////////////////
+
+
+// ship variable give position
+var ship = {
+  left: 575,
+  top: 750
+}
+
+// the missile
+var missles= [];
+
+
+// where the invaders will be
+
+
+var invaders= [
+  { left:200, top: 100 }, // 1
+  { left:300, top: 100 }, // 2
+  { left:400, top: 100 }, // 3
+  { left:500, top: 100 }, // 4
+  { left:600, top: 100 }, // 5
+  { left:700, top: 100 }, // 6
+  { left:800, top: 100 }, // 7
+  { left:900, top: 100 }, // 8
+  { left:200, top: 175 }, // 9
+  { left:200, top: 175 }, // 1
+  { left:300, top: 175 }, // 2
+  { left:400, top: 175 }, // 3
+  { left:500, top: 175 }, // 4
+  { left:600, top: 175 }, // 5
+  { left:700, top: 175 }, // 6
+  { left:800, top: 175 }, // 7
+  { left:900, top: 175 }, // 8
+
+];
+
+
+
+function drawShip() {
+  document.getElementById('ship').style.left = ship.left + 'px';
+  document.getElementById('ship').style.top = ship.top + 'px';
+}
+
+
+function drawMissiles() {
+  document.getElementById('missiles')
+}
+
+
+
+// get ship to move and fire missiles
+document.onKeyDown = function(e) {
+  if (e.keycode === 39) {
+    ship.left = ship.left + 10;
+  }
+  if (e.keycode === 37) {
+    ship.left = ship.left - 10;
+  }
+  if (e.keycode === 32) {
+    missiles.push( {
+      left: hero.left + 20,
+      top: hero.top - 20
+    });
+    drawMissiles()
+  }
+  drawShip();
 }
